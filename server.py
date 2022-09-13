6cea5ef06a9c2bd83c591a1e885d5b617d38b34c
Brian Dang <binhcdang.bd@gmail.com>
2022-09-13 11:05:47 +1000

    added cname record

--- server.py
+++ server.py
@@ -3,6 +3,11 @@ dns_table = {"www.google.com": "192.168.1.1",
              "www.deakin.com": "192.168.1.2",
              "www.instagram.com": "192.168.1.3",
              "www.linkedin.com": "192.168.1.4"}
+
+cname_record={"www.google.com": "host.google.com",
+              "www.deakin.com": "host.deakin.com",
+              "www.instagram.com": "host.instagram.com",
+              "www.linkedin.com": "host.linkedin.com"}
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print("Server is running")
 clientSocket.bind(("127.0.0.1", 1234))
@@ -12,4 +17,6 @@ while True:
     print(message)
     data = data.decode()
     ip = dns_table.get(data, "Data not found").encode()
+    cname = cname_record.get(data,"none").encode()
     send = clientSocket.sendto(ip, address)
+    send1 = clientSocket.sendto(cname,address)
